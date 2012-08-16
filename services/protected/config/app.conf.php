9c83983b6e509c942c5af7e20fa2c2344bd91fe6
Nelson Daza <nelson.daza@gmail.com>
2012-08-15 23:17:54 -0500

    Configuración de aplicación

--- services/protected/config/app.conf.php
+++ services/protected/config/app.conf.php
@@ -3,11 +3,11 @@
 //for setting collation and charset 'collate'=>'utf8_unicode_ci', 'charset'=>'utf8'
 //$dbconfig['dev'] = array('localhost', 'mydb', 'root', '1234', 'mysql', true, 'collate'=>'utf8_unicode_ci', 'charset'=>'utf8');
 $config['FACEBOOK'] = array( );
-$config['FACEBOOK']['CANVAS_PAGE'] = "http://idmeasure.onezink.com/";
-$config['FACEBOOK']['APP_ID'] = '206103019518328';
-$config['FACEBOOK']['APP_SECRET'] = '296903b8b4654449515dc5c259b90b69';
+$config['FACEBOOK']['CANVAS_PAGE'] = "canvas";
+$config['FACEBOOK']['APP_ID'] = '111111111';
+$config['FACEBOOK']['APP_SECRET'] = '222222222';
 
 
-$dbconfig['dev']  = array( '127.0.0.1', 'cnwpccj_idmeasure', 'cnwpccj_measure', 'mes0487', 'mysql', true, 'collate'=>'latin1_swedish_ci', 'charset'=>'latin1' );
-$dbconfig['prod'] = array( '127.0.0.1', 'cnwpccj_idmeasure', 'cnwpccj_measure', 'mes0487', 'mysql', true, 'collate'=>'latin1_swedish_ci', 'charset'=>'latin1' );
+$dbconfig['dev']  = array( '127.0.0.1', 'db', 'user', 'pass', 'mysql', true, 'collate'=>'latin1_swedish_ci', 'charset'=>'latin1' );
+$dbconfig['prod'] = array( '127.0.0.1', 'db', 'user', 'pass', 'mysql', true, 'collate'=>'latin1_swedish_ci', 'charset'=>'latin1' );
 
